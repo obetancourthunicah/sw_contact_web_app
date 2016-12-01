4eced44bc5f8bade0a6b584755b87dcb9107b1ec
Orlando J Betancourth <orlando.betancourth@gmail.com>
2016-11-30 23:49:29 -0600

    Esquema de Seguridad

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,12 +1,17 @@
 //Archivo de Control de la Aplicacion
 
 //Cuando JqueryMobile se ha cargado
-  //Funciones de Inicio
-  $("#contact_list").on('pagecreate', contact_list_onLoad );
-  $("#contact_detail").on('pagecreate', function(){ _dtlCargado = true;} );
-  $("#contact_list_mainlist").on('click','a',contact_list_mainList_onclick);
-  $("#btn_agregar_contact").on('click',nc_contact_onclick);
-  $("#btn_agregar_direccion").on('click',nd_direccion_onclick);
+//Funciones de Inicio
+
+
+$("#contact_list").on('pagecreate', contact_list_onLoad);
+$("#contact_detail").on('pagecreate', function() { _dtlCargado = true; });
+$("#contact_list_mainlist").on('click', 'a', contact_list_mainList_onclick);
+$("#btn_agregar_contact").on('click', nc_contact_onclick);
+$("#btn_agregar_direccion").on('click', nd_direccion_onclick);
+$("#btn_login").on('click', lg_login_onclick);
+$("#btn_register").on('click', lg_register_onclick);
+
 
 
 
@@ -15,92 +20,123 @@ var _currectPage = 1;
 var _numPersonas = 9999;
 var _currentContactId = "";
 var _dtlCargado = false;
-function contact_list_onLoad(e){
+//Para Manejar el tocken 
+var _tocken = "";
+var _tockenValidated = false;
+//verificamos si el browser soporta localstorage
+var _storageAvailable = (typeof(Storage) !== "undefined") ? true : false;
+
+
+function validateTocken() {
+    if (!_tockenValidated && _storageAvailable) {
+        //verificamos si existe algun tocken en la localstorage
+        if (localStorage._tocken) {
+            _tocken = localStorage._tocken;
+        }
+        _tockenAvailable = (_tocken) && true;
+    }
+    ajax_validate_token(function(err, data) {
+        if (err) {
+            _tocken = "";
+            _tockenValidated = false;
+            _tockenAvailable = false;
+            //esto elimina el sibolo del localStorage
+            delete localStorage._tocken;
+            changeTo("login_page");
+        } else {
+            _tockenValidated = true;
+            _tockenAvailable = true;
+            changeTo("contact_list");
+        }
+    })
+} //validateTocken
+
+function contact_list_onLoad(e) {
     obtenerPersonas(_currectPage, _numPersonas,
-        function(err, personas){
-          if(err){
-            return console.log("Error al Cargar Personas");
-          }
-          render_persona_list(personas);
+        function(err, personas) {
+            if (err) {
+                return console.log("Error al Cargar Personas");
+            }
+            render_persona_list(personas);
 
         }
-      );
+    );
 } //contact_list_onLoad
 
 
-  function render_persona_list(personas){
+function render_persona_list(personas) {
     var htmlstr = personas.map(
-      function(persona, i){
-        return '<li><a href="#contact_detail" data-id="'+persona._id+'">'+persona.nombre + " " + persona.apellido +'</a></li>';
-      }
+        function(persona, i) {
+            return '<li><a href="#contact_detail" data-id="' + persona._id + '">' + persona.nombre + " " + persona.apellido + '</a></li>';
+        }
     ).join("");
 
     $("#contact_list_mainlist").html(htmlstr).listview("refresh");
-  }
+}
 
-  function contact_list_mainList_onclick(e){
+function contact_list_mainList_onclick(e) {
     e.preventDefault();
     e.stopPropagation();
     var sender = $(this);
     _currentContactId = sender.data("id");
     console.log(_currentContactId);
     render_persona_actual();
-  }//contact_list_mainList_onclick
+} //contact_list_mainList_onclick
 
-  function render_persona_actual(){
-    obtenerPersonaActual(_currentContactId, function(err, persona){
-      if(err) return console.log("Error al cargar persona");
-      console.log(persona);
-      $("#dtlNombre").html(persona.nombre);
-      $("#dtlApellido").html(persona.apellido);
-      var direccionesHtml = "";
-      if(persona.direcciones && true){
-         direccionesHtml = persona.direcciones.map(
-            function(direccion,i){
-              return "<li><p>"+direccion.direccion+"</p><p>"+direccion.telefono+"</p><p>"+direccion.correo+"</p></li>";
-            }
-          ).join("");
+function render_persona_actual() {
+    obtenerPersonaActual(_currentContactId, function(err, persona) {
+        if (err) return console.log("Error al cargar persona");
+        console.log(persona);
+        $("#dtlNombre").html(persona.nombre);
+        $("#dtlApellido").html(persona.apellido);
+        var direccionesHtml = "";
+        if (persona.direcciones && true) {
+            direccionesHtml = persona.direcciones.map(
+                function(direccion, i) {
+                    return "<li><p>" + direccion.direccion + "</p><p>" + direccion.telefono + "</p><p>" + direccion.correo + "</p></li>";
+                }
+            ).join("");
         }
-      direccionesHtml += '<li><a href="#nueva_direccion">Agregar Dirección</a></li>';
-      console.log(direccionesHtml);
-      var lv = $("#dtlDirecciones").html(direccionesHtml);
-      if(_dtlCargado) lv.listview("refresh");
-      changeTo("contact_detail");
+        direccionesHtml += '<li><a href="#nueva_direccion">Agregar Dirección</a></li>';
+        console.log(direccionesHtml);
+        var lv = $("#dtlDirecciones").html(direccionesHtml);
+        if (_dtlCargado) lv.listview("refresh");
+        changeTo("contact_detail");
     });
-  }
+}
 
-  function nc_contact_onclick(e){
-      e.preventDefault();
-      e.stopPropagation();
+function nc_contact_onclick(e) {
+    e.preventDefault();
+    e.stopPropagation();
 
-      var _nombre = $("#nc_nombre").val();
-      var _apellido = $("#nc_apellido").val();
+    var _nombre = $("#nc_nombre").val();
+    var _apellido = $("#nc_apellido").val();
 
-      var form_body = {
+    var form_body = {
         "nombre": _nombre,
-        "apellido":_apellido
-      };
+        "apellido": _apellido
+    };
 
-      guardarNuevoContacto(form_body, function(err, data){
-        if(err){
-          return console.log("Error al guardar contacto");
-        }
-        $("#nc_nombre").val("");
-        $("#nc_apellido").val("");
+    guardarNuevoContacto(form_body, function(err, data) {
+            if (err) {
+                return console.log("Error al guardar contacto");
+            }
+            $("#nc_nombre").val("");
+            $("#nc_apellido").val("");
 
-        obtenerPersonas(_currectPage, _numPersonas,
-            function(err, personas){
-              if(err){
-                return console.log("Error al Cargar Personas");
-              }
-              render_persona_list(personas);
-              changeTo("contact_list");
-            } );//obtenerPersonas
-          }// guardarNuevoContacto
-      );
-  }//nc_contact_onclick
+            obtenerPersonas(_currectPage, _numPersonas,
+                function(err, personas) {
+                    if (err) {
+                        return console.log("Error al Cargar Personas");
+                    }
+                    render_persona_list(personas);
+                    changeTo("contact_list");
+                }); //obtenerPersonas
+        } // guardarNuevoContacto
+    );
+} //nc_contact_onclick
 
-  function nd_direccion_onclick(e){
+function nd_direccion_onclick(e) {
     e.preventDefault();
     e.stopPropagation();
 
@@ -108,105 +144,198 @@ function contact_list_onLoad(e){
     var _direccion = $("#nd_direccion").val();
     var _correo = $("#nd_correo").val();
 
-    var _form_body =  {
+    var _form_body = {
         "direccion": _direccion,
         "telefono": _telefono,
         "correo": _correo
     };
 
-  guardarNuevaDireccion(_currentContactId,_form_body, function(err, data){
-      if(err){
-        return console.log("Error al guardar la dirección");
-      }
-      $("#nd_telefono").val("");
-      $("#nd_direccion").val("");
-      $("#nd_correo").val("");
+    guardarNuevaDireccion(_currentContactId, _form_body, function(err, data) {
+        if (err) {
+            return console.log("Error al guardar la dirección");
+        }
+        $("#nd_telefono").val("");
+        $("#nd_direccion").val("");
+        $("#nd_correo").val("");
 
-      render_persona_actual();
+        render_persona_actual();
 
-    }
-  ); //guardarNuevaDireccion//;
+    }); //guardarNuevaDireccion//;
 
-  }
+} //
+
+function lg_login_onclick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var _correo = $("#lg_correo").val();
+    var _password = $("#lg_password").val();
+
+    var login_data = {
+        "correo": _correo,
+        "password": _password
+    };
+
+    login_ajax(login_data, function(err, data) {
+        if (err) {
+            alert("Error al validar credenciales");
+        } else {
+            _tocken = data._id;
+            _tockenValidated = true;
+            _tockenAvailable = true;
+            localStorage._tocken = _tocken;
+            changeTo("contact_list");
+        }
+    }); //login
+} //lg_login_onclick
+
+function lg_register_onclick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var _correo = $("#rg_correo").val();
+    var _password = $("#rg_password").val();
+    var _nombre = $("#rg_nombre").val();
+
+    var register_data = {
+        "correo": _correo,
+        "password": _password,
+        "nombre": _nombre
+    };
+
+    register_ajax(register_data, function(err, data) {
+        if (err) {
+            alert("Error al Registrar Usuario");
+        } else {
+            changeTo("login_page");
+            $("#rg_correo").val("");
+            $("#rg_password").val("");
+            $("#rg_nombre").val("");
+        }
+    }); //login
+} //lg_register_onclick
 
 //Ajax events
 //Configurar Ajax
 var settings = {
-  "async": true,
-  "crossDomain": true,
-  "dataType":"json",
-  "headers": {
-    "cache-control": "no-cache"
-  }
+    "async": true,
+    "crossDomain": true,
+    "dataType": "json",
+    "headers": {
+        "cache-control": "no-cache"
+    }
 }
 
 $.ajaxSetup(settings);
 
-function obtenerPersonas(page, numPersonas, despues){
-    $.ajax(
-      {
-        "url":"api/personas/"+page + "/" + numPersonas,
-        "method":"get",
-        "data":{},
-        "success": function(data, txtSuccess, xhrq){
-                        despues(null, data);
-                    },
-        "error": function(xhrq, errTxt, data){
-                        despues(true, null);
-                    }
-      }
-    );
+function login_ajax(form_data, despues) {
+    $.ajax({
+        "url": "api/usuario/login",
+        "method": "post",
+        "data": form_data,
+        "success": function(data, txtSuccess, xhrq) {
+            despues(null, data);
+        },
+        "error": function(xhrq, errTxt, data) {
+            despues(true, null);
+        }
+    });
+} //login_ajax
+
+function register_ajax(form_data, despues) {
+    $.ajax({
+        "url": "api/usuario/nuevo",
+        "method": "post",
+        "data": form_data,
+        "success": function(data, txtSuccess, xhrq) {
+            despues(null, data);
+        },
+        "error": function(xhrq, errTxt, data) {
+            despues(true, null);
+        }
+    });
+} //register_ajax
+
+function ajax_validate_token(despues) {
+    $.ajax({
+        "url": "/api/tocken/" + _tocken,
+        "method": "get",
+        "data": {},
+        "success": function(data, txtSuccess, xhrq) {
+            despues(null, data);
+        },
+        "error": function(xhrq, errTxt, data) {
+            despues(true, null);
+        }
+    });
+} // ajax_validate_token
+
+function obtenerPersonas(page, numPersonas, despues) {
+    $.ajax({
+        "url": "api/personas/" + page + "/" + numPersonas + "?tocken=" + _tocken,
+        "method": "get",
+        "data": {},
+        "success": function(data, txtSuccess, xhrq) {
+            despues(null, data);
+        },
+        "error": function(xhrq, errTxt, data) {
+            despues(true, null);
+        }
+    });
 } //obtenerPersonas
 
-function obtenerPersonaActual(currentid, despues){
-      $.ajax(
-        {
-          "url":"api/persona/" + currentid,
-          "method":"get",
-          "data":{},
-          "success": function(data, txtSuccess, xhrq){
-                          despues(null, data);
-                      },
-          "error": function(xhrq, errTxt, data){
-                          despues(true, null);
-                      }
+function obtenerPersonaActual(currentid, despues) {
+    $.ajax({
+        "url": "api/persona/" + currentid + "?tocken=" + _tocken,
+        "method": "get",
+        "data": {},
+        "success": function(data, txtSuccess, xhrq) {
+            despues(null, data);
+        },
+        "error": function(xhrq, errTxt, data) {
+            despues(true, null);
         }
-      );
+    });
 }
 
-function guardarNuevoContacto(form_data, despues){
-  $.ajax(
-    {
-      "url":"api/nueva_persona",
-      "method":"post",
-      "data":form_data,
-      "success": function(data, txtSuccess, xhrq){
-                      despues(null, data);
-                  },
-      "error": function(xhrq, errTxt, data){
-                      despues(true, null);
-                  }
-    }
-  );
+function guardarNuevoContacto(form_data, despues) {
+    $.ajax({
+        "url": "api/nueva_persona?tocken=" + _tocken,
+        "method": "post",
+        "data": form_data,
+        "success": function(data, txtSuccess, xhrq) {
+            despues(null, data);
+        },
+        "error": function(xhrq, errTxt, data) {
+            despues(true, null);
+        }
+    });
 };
 
-function guardarNuevaDireccion(current_id,form_data, despues){
-  $.ajax(
-    {
-      "url":"api/persona/"+ current_id,
-      "method":"put",
-      "data":form_data,
-      "success": function(data, txtSuccess, xhrq){
-                      despues(null, data);
-                  },
-      "error": function(xhrq, errTxt, data){
-                      despues(true, null);
-                  }
-    }
-  );
+function guardarNuevaDireccion(current_id, form_data, despues) {
+    $.ajax({
+        "url": "api/persona/" + current_id + "?tocken=" + _tocken,
+        "method": "put",
+        "data": form_data,
+        "success": function(data, txtSuccess, xhrq) {
+            despues(null, data);
+        },
+        "error": function(xhrq, errTxt, data) {
+            despues(true, null);
+        }
+    });
 };
 
 //utilities
-function changeTo(to){
-  $(":mobile-pagecontainer").pagecontainer("change","#" + to);
+function changeTo(to) {
+    $(":mobile-pagecontainer").pagecontainer("change", "#" + to);
 }
+
+//funciones que se corren al inicio
+
+function init_spa(e) {
+    console.log("Inició Mobile SPA");
+    validateTocken();
+}
+
+init_spa();
--- routes/api.js
+++ routes/api.js
@@ -2,87 +2,167 @@ var express = require('express');
 var router = express.Router();
 var assign = require('object-assign');
 var ObjectID = require('mongodb').ObjectID;
-function apiFactory(db){
-  var personasColl = db.collection('personas');
 
-  router.post('/nueva_persona', function(req,res,next){
-    var newPersona = assign({},{"nombre":"","apellido":""},req.body);
-    personasColl.insert(newPersona, function(err, rslt){
-      if(err){
-        return res.status(400).json(err);
-        //res.status(400).json({"error":"No se puede insertar!"});
-      }
-      res.status(200).json(newPersona);
-    }
-    );
-  });
-  router.get('/personas', function(req,res,next){
-    personasColl.find({}).toArray(function(err,personas){
-      if(err){
-        return res.status(400).json([]);
-      }
-      res.status(200).json(personas);
-    });
-  });
+function apiFactory(db) {
+    var personasColl = db.collection('personas');
+    var usuariosColl = db.collection('usuarios');
+    var tockensColl = db.collection('tockens');
 
-  router.get('/persona/:id', function(req,res,next){
-      var id = ObjectID(req.params.id);
-      personasColl.findOne({"_id":id}, function(err,persona){
-        if(err){
-          console.log(err);
-          return res.status(400).json({"error":"Error al cargar la persona"});
+    //registrarusuario
+    router.post('/usuario/nuevo', function(req, res, next) {
+        var newPersona = { "correo": "", "nombre": "", "password": "" };
+        newPersona = assign({}, newPersona, req.body);
+        usuariosColl.insert(newPersona, function(err, rsl) {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ "error": "No se pudo insertar usuario" });
+            }
+            res.status(200).json({ "status": "ok" });
+        });
+    }); // /usuario/nuevo
+
+    router.post('/usuario/login', function(req, res, next) {
+        var credenciales = { "correo": "", "password": "" };
+        credenciales = assign({}, credenciales, req.body);
+        var query = { "correo": credenciales.correo };
+        usuariosColl.findOne(query, function(err, usuario) {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ "error": "No se pudo encontrar usuario" });
+            }
+            if (!usuario) {
+                return res.status(400).json({ "error": "No se pudo encontrar usuario" });
+            }
+            console.log(usuario);
+            console.log(credenciales);
+            if (usuario.password != credenciales.password) {
+                return res.status(400).json({ "error": "Credenciales no son válidas" });
+            }
+            var newTocken = { "correo": credenciales.correo, "fecha": (Date.now() + (1000 * 60 * 60 * 24 * 30)) };
+
+            tockensColl.insert(newTocken, function(err, rslt) {
+                if (err) {
+                    return res.status(400).json({ "error": "No se pudo generar tocken" });
+                }
+                res.status(200).json(newTocken);
+            });
+
+        });
+    }); //login
+
+
+    //validaruntocken
+    router.get('/tocken/:tk', function(req, res, next) {
+        if (!req.params.tk) {
+            return res.status(400).json({ "error": "tocken no es válido" });
         }
-        res.status(200).json(persona);
-      });
-  });
+        var query = { "_id": ObjectID(req.params.tk), "fecha": { "$gte": Date.now() } };
+        tockensColl.findOne(query, function(err, tockenDoc) {
+            if (err) {
+                return res.status(400).json({ "error": "tocken no es válido" });
+            }
+            if (!tockenDoc) {
+                return res.status(400).json({ "error": "tocken no es válido" });
+            }
+            res.status(200).json({ "status": "ok" });
+        });
+    }); //tocken
 
-  router.put('/persona/:id', function(req,res,next){
-      var id = ObjectID(req.params.id);
-      var updateAddress = assign({},
-                                  {"direccion":"",
-                                  "telefono":"",
-                                  "correo":""},
-                                  req.body
-                                );
-        var updateExpression = {"$push":
-                                  {"direcciones":updateAddress},
-                                "$inc":
-                                  {"numDirecciones":1}
-                                };
-        personasColl.update({"_id":id},updateExpression, function(err,rlt){
-            if(err){
-              console.log(err);
-              return res.status(400).json({"error":"No se pudo actualizar persona"});
+    function validateTockenInternal(req, res, next) {
+        var tocken = req.query.tocken;
+        if (!tocken) {
+            return res.status(400).json({ "error": "Acceso denegado" });
+        }
+        var tockenFilter = { "_id": ObjectID(tocken) };
+        tockensColl.findOne(tockenFilter, function(err, tockenDoc) {
+            if (err) {
+                return res.status(400).json({ "error": "Acceso Denegado" });
+            }
+            if (!tockenDoc) {
+                return res.status(400).json({ "error": "Acceso Denegado" });
+            }
+            next();
+        });
+    }
+
+
+    router.post('/nueva_persona', validateTockenInternal, function(req, res, next) {
+        var newPersona = assign({}, { "nombre": "", "apellido": "" }, req.body);
+        personasColl.insert(newPersona, function(err, rslt) {
+            if (err) {
+                return res.status(400).json(err);
+                //res.status(400).json({"error":"No se puede insertar!"});
+            }
+            res.status(200).json(newPersona);
+        });
+    });
+    router.get('/personas', validateTockenInternal, function(req, res, next) {
+        personasColl.find({}).toArray(function(err, personas) {
+            if (err) {
+                return res.status(400).json([]);
+            }
+            res.status(200).json(personas);
+        });
+    });
+
+    router.get('/persona/:id', validateTockenInternal, function(req, res, next) {
+        var id = ObjectID(req.params.id);
+        personasColl.findOne({ "_id": id }, function(err, persona) {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ "error": "Error al cargar la persona" });
+            }
+            res.status(200).json(persona);
+        });
+    });
+
+    router.put('/persona/:id', validateTockenInternal, function(req, res, next) {
+        var id = ObjectID(req.params.id);
+        var updateAddress = assign({}, {
+                "direccion": "",
+                "telefono": "",
+                "correo": ""
+            },
+            req.body
+        );
+        var updateExpression = {
+            "$push": { "direcciones": updateAddress },
+            "$inc": { "numDirecciones": 1 }
+        };
+        personasColl.update({ "_id": id }, updateExpression, function(err, rlt) {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ "error": "No se pudo actualizar persona" });
             }
             //console.log(rlt.result);
-            if(rlt.result.nModified){
-                res.status(200).json({"status":"ok"});
-            }else{
-              res.status(400).json({"error":"No se modificó la persona seleccionada"});
+            if (rlt.result.nModified) {
+                res.status(200).json({ "status": "ok" });
+            } else {
+                res.status(400).json({ "error": "No se modificó la persona seleccionada" });
             }
 
         });
-  });
+    });
 
-  router.get('/personas/:pagina/:documentos', function(req,res,next){
-      var paginaReal = parseInt(req.params.pagina) -1;
-      if(paginaReal <0) paginaReal = 0;
-      var documentos = parseInt(req.params.documentos);
-      if (documentos < 5) documentos  =5;
-      personasColl.find()
-                  .project({"nombre":1,"apellido":1})
-                  .sort({"nombre":1})
-                  .skip((paginaReal * documentos))
-                  .limit(documentos)
-                  .toArray(function(err, personas){
-                    if(err){
-                      return res.status(400).json({"error":"Error al obtener personas"});
-                    }
-                    res.status(200).json(personas);
-                  });
-  });
+    router.get('/personas/:pagina/:documentos', validateTockenInternal, function(req, res, next) {
+        var paginaReal = parseInt(req.params.pagina) - 1;
+        if (paginaReal < 0) paginaReal = 0;
+        var documentos = parseInt(req.params.documentos);
+        if (documentos < 5) documentos = 5;
+        personasColl.find()
+            .project({ "nombre": 1, "apellido": 1 })
+            .sort({ "nombre": 1 })
+            .skip((paginaReal * documentos))
+            .limit(documentos)
+            .toArray(function(err, personas) {
+                if (err) {
+                    return res.status(400).json({ "error": "Error al obtener personas" });
+                }
+                res.status(200).json(personas);
+            });
+    });
 
- return router;
+    return router;
 } //apiFactory
 
 module.exports = apiFactory;
